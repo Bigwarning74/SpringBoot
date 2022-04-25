1f6e2c6b64da1b5be9b227663619d5969a4eb9e4
Kasyo <1912292065@qq.com>
2022-04-25 22:12:39 +0800

    初始化项目

---
+++ src/main/java/cn/edu/scnu/Starter.java
@@ -0,0 +1,12 @@
+package cn.edu.scnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter {
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+    }
+
+}
